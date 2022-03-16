f9fecf606d9e15c61e01ee6427bb676f7293b80b
honorezemagho <honorezemagho@gmail.com>
2022-03-16 14:26:56 -0700

    updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
-int main() { 
-   char *ch = "_putchar"; 
-   while (*ch) { 
-       _putchar(*ch);
-        ch++;
-   }
-     _putchar('\n');
+int main() {
+ char *ch = "_putchar";
+ while (*ch) { 
+   _putchar(*ch);
+   ch++;
+ }
+ _putchar('\n');
  return (0);
 }
