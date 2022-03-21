abde8456330097829b25a804792354ccf2c30085
honorezemagho <honorezemagho@gmail.com>
2022-03-21 12:44:10 -0700

    updates

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-   [a,b] = [b,a]
-}
+   int a_original = *a;
+   int b_original = *b;
 
+   *a = b_original;
+   *b = a_original;
+}
